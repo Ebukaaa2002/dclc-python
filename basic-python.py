1eab6a34c47eba3fa31b8704e8b0cf60de706267
Miracle Agwuegbo <magwuegbo@yahoo.com>
2026-03-31 07:35:02 +0100

    completed exercise 2

--- basic-python.py
+++ basic-python.py
@@ -1,8 +1,16 @@
+
+#Exercise 1
 num1 = 30
 num2 = 70
 product = num1 * num2
 sum = num1 + num2
 if product > 100:
-    print ( 'The product is:', product)
+    print ('The product is:', product)
 else:
-    print ( 'The sum is:', sum) 
+    print ('The sum is:', sum) 
+
+
+
+#Exercise 2
+for i in range(1, 11):
+    print(i, '+', (i-1), '=', i + (i-1))
