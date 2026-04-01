b3079140d85bc11965df310b170ceba9f351130f
Miracle Agwuegbo <magwuegbo@yahoo.com>
2026-04-01 06:48:13 +0100

    completed data structures exercise 2

--- data-structures.py
+++ data-structures.py
@@ -7,3 +7,16 @@ even_elements = list2[0::2]
 
 result = odd_elements + even_elements
 print("Final list:", result)
+
+# Exercise 2 - Remove item at index 4, add to 2nd position and end
+sample_list = [34, 54, 67, 89, 11, 43, 94]
+print("Original list:", sample_list)
+
+element = sample_list.pop(4)
+print("List after removing index 4:", sample_list)
+
+sample_list.insert(2, element)
+print("List after adding at index 2:", sample_list)
+
+sample_list.append(element)
+print("List after adding at end:", sample_list)
