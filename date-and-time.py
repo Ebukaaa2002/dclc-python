dd74eafc2308a4d390f9d5cc3686b59305907b1a
Miracle Agwuegbo <magwuegbo@yahoo.com>
2026-04-06 21:44:44 +0100

    completed date and time exercise 2

--- date-and-time.py
+++ date-and-time.py
@@ -6,3 +6,10 @@ print("Current date and time:", datetime.datetime.now())
 
 # Print only current time
 print("Current time:", datetime.datetime.now().time())
+
+# Exercise 2 - Convert string into datetime object
+from datetime import datetime
+
+date_string = "25 February, 2020"
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+print("Date object:", date_object)
