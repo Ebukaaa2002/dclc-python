597caf7e14d0e3920cf4cef0cedcd724561b5aff
Miracle Agwuegbo <magwuegbo@yahoo.com>
2026-04-06 21:41:39 +0100

    completed date and time exercise 1

---
+++ date-and-time.py
@@ -0,0 +1,8 @@
+# Exercise 1 - Print current date and time
+import datetime
+
+# Print current date and time
+print("Current date and time:", datetime.datetime.now())
+
+# Print only current time
+print("Current time:", datetime.datetime.now().time())
